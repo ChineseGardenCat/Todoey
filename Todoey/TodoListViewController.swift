eb8cb730cb2c477f1eeeca148c0fba67898b3870
腾宇Matthew <31982122+ChineseGardenCat@users.noreply.github.com>
2019-02-03 11:31:33 +1100

    Basic functions: display items on table view, select and mark

---
+++ Todoey/TodoListViewController.swift
@@ -0,0 +1,57 @@
+//
+//  ViewController.swift
+//  Todoey
+//
+//  Created by MATTEW MA on 3/2/19.
+//  Copyright © 2019 Mathhew MA. All rights reserved.
+//
+
+import UIKit
+
+class TodoListViewController: UITableViewController {
+    
+    let toDoListItemArray = ["Find Mike","Buy Eggs","Destory Demogorgon"]
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+    //MARK - Tableview Datasource Methods
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoItemCell", for: indexPath)
+        
+        cell.textLabel?.text = toDoListItemArray[indexPath.row]
+        
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        return toDoListItemArray.count
+    
+    }
+    
+    //MARK - TableView Delegate Methods
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+        print(tableView.cellForRow(at: indexPath)!.textLabel!.text!)
+        
+        if tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark{
+             tableView.cellForRow(at: indexPath)?.accessoryType = .none
+        }else{
+             tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
+        }
+        
+        tableView.deselectRow(at: indexPath, animated: true)
+        
+        
+    }
+    
+    
+
+}
+
